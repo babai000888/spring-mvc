fe2fb9b885ba9709e30d113e4f04b7d04853e706
babai000888 <47792371+babai000888@users.noreply.github.com>
2021-08-17 23:56:22 +0300

    Доделал

--- 2.2.2. spring_mvc/src/main/java/web/MainApp.java
+++ 2.2.2. spring_mvc/src/main/java/web/MainApp.java
@@ -8,19 +8,18 @@ import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
-
-        List<Car> carList = Arrays.asList(
+        List<Car> carList = getCars();
+        for (Car car : new CarService().getNcars(carList,3)) {
+            System.out.println( car.toString() );
+        }
+    }
+    public static List<Car> getCars() {
+        return  Arrays.asList(
                 new Car("Lada", 2101, 1965),
                 new Car("Ford", 999, 1950),
                 new Car("Fiat", 123, 2001),
                 new Car("Audi", 100, 1985),
                 new Car("BMW", 3, 1990));
-
-        for (Car car : new CarService().getNcars(carList,3)) {
-            System.out.println( car.toString() );
-        }
-
-
     }
 }
 
--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -3,20 +3,25 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.MainApp;
+import web.model.Car;
+import web.service.CarService;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @GetMapping("/car")
-    public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("5.CARRRRR ");
-        model.addAttribute("messages", messages);
-        return "index";
+    @GetMapping("/cars")
+    public String printCars(ModelMap model, @RequestParam("count") int count) {
+        List<Car> carList = MainApp.getCars();
+        if(carList.size() < count) { count = carList.size(); }
+        List<Car> cars = new CarService().getNcars(carList,count);
+        model.addAttribute("cars",cars);
+        return "cars";
     }
 
 }
