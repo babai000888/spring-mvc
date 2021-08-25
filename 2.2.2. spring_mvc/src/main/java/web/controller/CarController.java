d361dba288e8aef23d68112b5924014c6ce8875a
babai000888 <47792371+babai000888@users.noreply.github.com>
2021-08-25 03:26:42 +0300

    - добавил проверку  n < 0 | n > 4 - убрал MainApp.java

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -4,23 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.MainApp;
-import web.model.Car;
 import web.service.CarService;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class CarController {
 
     @GetMapping("/cars")
     public String printCars(ModelMap model, @RequestParam(defaultValue = "100") int count) {
-        List<Car> carList = MainApp.getCars();
-        List<Car> cars = new CarService().getNcars(carList,count);
-        model.addAttribute("cars",cars);
+        model.addAttribute("cars",new CarService().getNcars(CarService.getCars(),count));
         return "cars";
     }
-
 }
--- 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
@@ -2,11 +2,22 @@ package web.service;
 
 import web.model.Car;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CarService {
+
+    public static List<Car> getCars() {
+        return  Arrays.asList(
+                new Car("Lada", 2101, 1965),
+                new Car("Ford", 999, 1950),
+                new Car("Fiat", 123, 2001),
+                new Car("Audi", 100, 1985),
+                new Car("BMW", 3, 1990));
+    }
+
     public List<Car> getNcars (List<Car> carList, int n) {
-        if(carList.size() < n) { n = carList.size(); }
+        if(carList.size() < n | n < 0 | n > 4) { n = carList.size(); }
         return carList.subList(0,n);
     }
 }
